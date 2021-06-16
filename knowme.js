6f5c5010b137b59e8d940a13e6a1d780744437c1
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-06-16 10:00:24 +0530

    Added a function with scores to be updated

--- knowme.js
+++ knowme.js
@@ -2,3 +2,17 @@ var readlineSync = require("readline-sync");
 var score = 0;
 var userName = readlineSync.question("What is your name?");
 console.log("Welcome" + userName + "! \nLet's check how much you know Jaahanava Joshi");
+
+function play(question,answer) {
+    var userAnswer = readlineSync.question(question);
+  
+    if(userAnswer == answer) {
+      score = score + 1;
+      console.log("You are right!");
+      console.log("Your score is: "+score);
+    }
+    else {
+      console.log("You are wrong!");
+      console.log("Your score is: "+score);
+    }
+  }
