91f8cc2d64cd989fe5a2eeaaa7eaaf246075cc58
Jaahanava Joshi <jaahanavajoshi@gmail.com>
2021-06-16 09:58:48 +0530

    Taking username and welcome message

---
+++ knowme.js
@@ -0,0 +1,4 @@
+var readlineSync = require("readline-sync");
+var score = 0;
+var userName = readlineSync.question("What is your name?");
+console.log("Welcome" + userName + "! \nLet's check how much you know Jaahanava Joshi");
